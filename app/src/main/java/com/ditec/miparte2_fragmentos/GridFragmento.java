2ae0aec22c0bbd078b4313339790693289fb672f
Yennyqm <yennyjulissa@gmail.com>
2021-03-12 22:06:09 -0500

    Añadiendo caso dinamico del fragmento

--- app/src/main/java/com/ditec/miparte2_fragmentos/GridFragmento.java
+++ app/src/main/java/com/ditec/miparte2_fragmentos/GridFragmento.java
@@ -12,9 +12,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class GridFragmento extends Fragment { @Nullable
-@Override
-public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) { View view= inflater.inflate(R.layout.grid_fragmento,container,false);
+public class GridFragmento extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    View view= inflater.inflate(R.layout.grid_fragmento,container,false);
     GridView gv = view.findViewById(R.id.migridview);
     ArrayAdapter miarrayadapter= ArrayAdapter.createFromResource(getActivity(),R.array.lista,android.R.layout.simple_list_item_1); gv.setAdapter(miarrayadapter);
     return view;
