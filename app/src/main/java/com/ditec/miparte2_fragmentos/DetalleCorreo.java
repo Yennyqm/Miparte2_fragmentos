f1976cc461839cda82cb728393b6dd958ec1a48b
Yennyqm <yennyjulissa@gmail.com>
2021-03-19 19:17:03 -0500

    Listando correos con un recyclerview, usando un pojo de datos y cardview, enviando parametros al detallecorreo para visualizarlos en un textview

---
+++ app/src/main/java/com/ditec/miparte2_fragmentos/DetalleCorreo.java
@@ -0,0 +1,20 @@
+package com.ditec.miparte2_fragmentos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetalleCorreo extends AppCompatActivity {
+
+    TextView txtdetalle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_correo);
+        txtdetalle=findViewById(R.id.TxtDetalle);
+        txtdetalle.setText("De: "+getIntent().getExtras().getString("de")+
+                "Asunto: "+getIntent().getExtras().getString("asunto")+
+                "Contenido: "+getIntent().getExtras().getString("texto"));
+    }
+}
